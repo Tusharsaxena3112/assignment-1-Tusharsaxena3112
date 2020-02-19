d3b6960987bd0d340f76949bba7fa94d2aaf3923
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-02-19 22:15:22 +0530

    creating interface for the App

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,5 +1,7 @@
 package Main;
 
+import java.util.Scanner;
+
 public class Main {
 
     //Press 1 to add a new contact
@@ -9,8 +11,27 @@ public class Main {
     //Press 5 to exit program
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hey there !! My name is Tushar Saxena " +
                 ".. Here what can I do for you--->>");
-        System.out.println();
+        while(true) {
+            System.out.println("Press 1 to add a new contact");
+            System.out.println("Press 2 to view all contacts");
+            System.out.println("Press 3 to search for a contact");
+            System.out.println("Press 4 to delete a contact");
+            System.out.println("Press 5 to exit program ");
+
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1"://need to add function
+                case "2"://need to add function
+                case "3"://need to add function
+                case "4"://need to add function
+                case "5"://need to add function
+
+            }
+        }
+
     }
 }
