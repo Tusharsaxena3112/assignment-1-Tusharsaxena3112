e5417ee7a477e2367aca4fd136ad91e5d11da22b
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-02-19 20:01:10 +0530

    adding methods to Person.java

--- src/person/Person.java
+++ src/person/Person.java
@@ -1,4 +1,54 @@
 package person;
 
+import java.util.ArrayList;
+
 public class Person {
+    // a field to store the first name
+    // a field to store the last name
+    // a list to store multiple contact numbers (i.e a list of numbers)
+    // a field to store the email address of the person
+
+    private String firstName;
+    private String lastName;
+    private ArrayList<Integer> contacts;
+    private String email;
+
+    public Person(String firstName,String lastName,ArrayList<Integer> contacts,String email){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.contacts=contacts;
+        this.email=email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public ArrayList<Integer> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(ArrayList<Integer> contacts) {
+        this.contacts = contacts;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
