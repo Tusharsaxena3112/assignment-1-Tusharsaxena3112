33d45dee3b0b5f4d9f18871816cb5c99fe001c7c
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-02-19 20:05:42 +0530

    adding Main.java

---
+++ src/Main/Main.java
@@ -0,0 +1,16 @@
+package Main;
+
+public class Main {
+
+    //Press 1 to add a new contact
+    //Press 2 to view all contacts
+    //Press 3 to search for a contact
+    //Press 4 to delete a contact
+    //Press 5 to exit program
+
+    public static void main(String[] args) {
+        System.out.println("Hey there !! My name is Tushar Saxena " +
+                ".. Here what can I do for you--->>");
+        System.out.println();
+    }
+}
