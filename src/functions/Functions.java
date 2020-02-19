05ea0311c674dbe9b7df7cd330f6e54813431ccf
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-02-19 22:44:52 +0530

    adding addContact() method to Fucntions.java

--- src/functions/Functions.java
+++ src/functions/Functions.java
@@ -5,11 +5,9 @@ import person.Person;
 
 public class Functions {
 
-    public void addLast(LinkedList linkedList, Person person){
+    public void addContact(LinkedList linkedList, Person person){
         linkedList.addLast(person);
         System.out.println(person.getFirstName()+" "+person.getLastName() +"added to your contacts");
     }
-
-
 
 }
